69231c19cd9191585a4637e587c63c980db0373a
everton.silva <everton.silva@alicredito.com.br>
2021-09-01 15:19:03 -0300

    Curso MVC e API em CSharp e Dotnet

---
+++ CursoMVC/database/dbo.Categorias.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Categorias] ON
+INSERT INTO [dbo].[Categorias] ([Id], [Descricao]) VALUES (2, N'Categoria 1')
+INSERT INTO [dbo].[Categorias] ([Id], [Descricao]) VALUES (3, N'Categoria 2')
+SET IDENTITY_INSERT [dbo].[Categorias] OFF
